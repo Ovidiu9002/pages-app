7633269fad5a9040608ed02d0b04516045ecc196
Ovidiu9002 <marculescu.ovidiu90@gmail.com>
2021-03-20 15:35:39 +0200

    Req.txt added

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn --chdir pagesProject pagesProject.wsgi:pages-app --log-file -
+web: python manage.py runserver
--- pagesProject/settings.py
+++ pagesProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-mra@0)=4!+sdq^syzw##6kh7ttux=p9jciti)@fjt&m%@=skv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['* ']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages-app',
+    'pages-app.PagesConfig',
 ]
 
 MIDDLEWARE = [
